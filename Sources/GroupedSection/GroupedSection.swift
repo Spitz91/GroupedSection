1d9c4f6568a6509a3b7f94f20eddea5f3c68d5e6
Marco Spitzenpfeil <>
2019-09-25 17:07:34 +0200

    public function

--- Sources/GroupedSection/GroupedSection.swift
+++ Sources/GroupedSection/GroupedSection.swift
@@ -3,7 +3,7 @@ public struct GroupedSection<Identifier: Hashable, Row> {
     var identifier : Identifier
     var rows : [Row]
 
-    static func group(_ rows : [Row], by indentifierForRow: (Row) -> Identifier) -> [GroupedSection<Identifier, Row>] {
+    public static func group(_ rows : [Row], by indentifierForRow: (Row) -> Identifier) -> [GroupedSection<Identifier, Row>] {
         return Dictionary(grouping: rows, by: indentifierForRow)
             .map(GroupedSection.init)
     }
